df9f5f951568b793e252ddda705aa020ba995c7d
sashatemereva13 <sashatemereva13@gmail.com>
2025-12-24 18:39:02 +0500

    added tailwind and adjusted css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,21 @@ export default function App() {
   });
 
   return (
-    <div>
-      <h1>Spotify Visualizer</h1>
+    <div className="relative w-screen h-screen overflow-hidden bg-black">
+
+<div className="absolute inset-0 z-0">
+       <VisualizerBasic data={analysis} />
+</div>
+
+<div className="relative z-10 flex flex-col h-full justify-between p-6 text-white">
+
+      <h1 className="font-display text-3xl">Spotify Visualizer</h1>
+
+      <div className="font-primary">
       <AudioPlayer />
-      <VisualizerBasic data={analysis} />
+      </div>
+      </div>
+ 
     </div>
   );
 }
